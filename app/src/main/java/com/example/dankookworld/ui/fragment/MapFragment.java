d43b444d65a0d44e40d6ff7a4027e77c6e7df2e2
psh <qkrtjdgus268@naver.com>
2019-11-04 23:31:26 +0900

    지도 프래그먼트 첫번째 시도를 위한 백업

--- app/src/main/java/com/example/dankookworld/ui/fragment/MapFragment.java
+++ app/src/main/java/com/example/dankookworld/ui/fragment/MapFragment.java
@@ -9,15 +9,65 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.dankookworld.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mMap;
+    private MapView mapView;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map_at, container, false);
+        mapView = (MapView) view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
 
-        return  view;
+        mapView.getMapAsync(this);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        MapsInitializer.initialize(this.getActivity());
+
+        // Updates the location and zoom of the MapView
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(35.141233, 126.925594), 14);
+
+        googleMap.animateCamera(cameraUpdate);
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(35.141233, 126.925594))
+                .title("루프리코리아"));
 
     }
 }
+
