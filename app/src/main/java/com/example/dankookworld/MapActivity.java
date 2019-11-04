91c3372331b2b797deb8c1f0acca409c4a3bc503
psh <qkrtjdgus268@naver.com>
2019-11-04 19:31:06 +0900

    회원정보 연동가능하게 수정(순서 수정)

---
+++ app/src/main/java/com/example/dankookworld/MapActivity.java
@@ -0,0 +1,13 @@
+package com.example.dankookworld;
+
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        
+    }
+}
