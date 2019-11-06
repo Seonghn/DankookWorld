b6297a7e511f692a3d7c84c40f09ee7297847916
박수연 <tndus0729@naver.com>
2019-11-06 15:15:02 +0900

    home, activity_main2 수정

--- app/src/main/java/com/example/dankookworld/C_finder.java
+++ app/src/main/java/com/example/dankookworld/C_finder.java
@@ -8,6 +8,6 @@ public class C_finder extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_c_finder);
+        setContentView(R.layout.fragment_find);
     }
 }
