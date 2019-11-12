d699bdad38d141b63d6f3ffdabc6d1b652b682d6
박수연 <tndus0729@naver.com>
2019-11-12 15:11:07 +0900

    mapfragment_information, store, wait 마커 추가

--- app/src/main/java/com/example/dankookworld/ui/fragment/MapFragment_information.java
+++ app/src/main/java/com/example/dankookworld/ui/fragment/MapFragment_information.java
@@ -43,6 +43,85 @@ public class MapFragment_information extends Fragment implements OnMapReadyCallb
         markerOptions.position(DW);
         markerOptions.title("단국대학교");
         markerOptions.snippet("범정관");
+
+        LatLng l1 = new LatLng(37.322655, 127.126027);
+        MarkerOptions m1 = new MarkerOptions();
+        m1.position(l1);
+        m1.title("정문 인포메이션");
+        mMap.addMarker(m1);
+
+        LatLng l2 = new LatLng(37.321276, 127.127455);
+        MarkerOptions m2 = new MarkerOptions();
+        m2.position(l1);
+        m2.title("중앙 인포메이션");
+        mMap.addMarker(m2);
+
+        LatLng l3 = new LatLng(37.319953, 127.125588);
+        MarkerOptions m3 = new MarkerOptions();
+        m3.position(l3);
+        m3.title("인포메이션 동");
+        mMap.addMarker(m3);
+
+        LatLng l4 = new LatLng(37.322308, 127.128989);
+        MarkerOptions m4 = new MarkerOptions();
+        m4.position(l4);
+        m4.title("인포메이션 서");
+        mMap.addMarker(m4);
+
+        LatLng l5 = new LatLng(37.321856, 127.124869);
+        MarkerOptions m5 = new MarkerOptions();
+        m5.position(l5);
+        m5.title("화장실1");
+        mMap.addMarker(m5);
+
+        LatLng l6 = new LatLng(37.322744, 127.127369);
+        MarkerOptions m6 = new MarkerOptions();
+        m6.position(l6);
+        m6.title("화장실2");
+        mMap.addMarker(m6);
+
+        LatLng l7 = new LatLng(37.320482, 127.126232);
+        MarkerOptions m7 = new MarkerOptions();
+        m7.position(l7);
+        m7.title("화장실3");
+        mMap.addMarker(m7);
+
+        LatLng l8 = new LatLng(37.321737, 127.129064);
+        MarkerOptions m8 = new MarkerOptions();
+        m8.position(l8);
+        m8.title("화장실4");
+        mMap.addMarker(m8);
+
+        LatLng l9 = new LatLng(37.320354, 127.128474);
+        MarkerOptions m9 = new MarkerOptions();
+        m9.position(l9);
+        m9.title("물품보관함1");
+        mMap.addMarker(m9);
+
+        LatLng l10 = new LatLng(37.321208, 127.125373);
+        MarkerOptions m10 = new MarkerOptions();
+        m10.position(l10);
+        m10.title("물품보관함2");
+        mMap.addMarker(m10);
+
+        LatLng l11 = new LatLng(37.321909, 127.126950);
+        MarkerOptions m11 = new MarkerOptions();
+        m11.position(l11);
+        m11.title("미아보호소");
+        mMap.addMarker(m11);
+
+        LatLng l12 = new LatLng(37.321505, 127.123702);
+        MarkerOptions m12 = new MarkerOptions();
+        m12.position(l12);
+        m12.title("서매표소");
+        mMap.addMarker(m12);
+
+        LatLng l13 = new LatLng(37.320812, 127.125808);
+        MarkerOptions m13 = new MarkerOptions();
+        m13.position(l13);
+        m13.title("동매표소");
+        mMap.addMarker(m13);
+
         mMap.addMarker(markerOptions);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(DW,17));
         mMap.setMyLocationEnabled(true);
